9d9c2ba283a58e4cea76b79dc4a42403bb042777
akhileshgoud <akhilesh.gounikadi@xcubelabs.com>
2022-12-14 17:57:21 +0530

    [TASK]: static and dynamic route navigation with link

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,5 +1,32 @@
+import Link from "next/link";
+
 export default function Home() {
+  const blog = [
+    {link:'first',name:'blog post first'},
+    {link:'second',name:'blog post second'},
+    {link:'third',name:'blog post third'}
+  ]
   return (
-    <h1>Hello Next JS</h1>
+    <div>
+      <h1>Hello Next JS</h1>
+      <ul>
+      <Link href='portfolio'>Portfolio</Link>
+      </ul>
+      <ul>
+      <Link href='about'>About</Link>
+      </ul>
+      <ul>
+      <Link href='client'>Client</Link>
+      </ul>
+      <ul>
+        {blog.map((details:any)=>{
+          return (
+            <li>
+            <Link href={`client/${details?.link}`}>{details?.name}</Link>
+            </li>
+          )
+        })}
+      </ul>
+    </div>
   )
 }
